1e83fcb8fe415d850d4dab4c4fd63e6b95419330
jamanal.smith@gmail.com <jamanal.smith@gmail.com@95c672d9-0ea3-1a43-bfba-4a9dfb2bd3a4>
2010-11-27 21:04:46 +0000

    made changes to linguistics Main

--- trunk/rdses-jms/src/main/java/ua/kiev/univ/jms/impl/ProcessedDocument.java
+++ trunk/rdses-jms/src/main/java/ua/kiev/univ/jms/impl/ProcessedDocument.java
@@ -6,19 +6,16 @@ import ua.kiev.univ.jms.Document;
  * @author jamanal
  * @version 2010-11-08
  */
-public class ProcessedDocument extends Document {
+public class ProcessedDocument extends RawDocument {
 
     private String group;
 
-    public ProcessedDocument(String name, byte[] data, String comment) {
-        super(name, data);
+    public ProcessedDocument(String name, byte[] data, String source, String group) {
+        super(name, data, source);
+        this.group = group;
     }
 
     public String getGroup() {
         return group;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
     }
 }
